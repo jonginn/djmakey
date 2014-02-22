a32f4a009a7a3528bac67d66ee4b8ee17953ff36
Jonathan Ginn <jon@jonginn.com>
2014-02-22 16:42:01 +0000

    front endizle

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <title>DJ MAKEY MAKEY</title>
+  <link href='http://fonts.googleapis.com/css?family=Archivo+Black' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/style.css">
   <?php
   $dir_handle = './sound';
   $sounds = array_diff(scandir($dir_handle), array('.', '..', 'index.php', '.DS_Store', 'dj', '.gitignore'));
@@ -11,8 +13,37 @@
 </head>
 <body>
 
-  <p class="keyCode"></p>
-  <p class="noise"></p>
+
+  <div class="container">
+    <div class="row">
+      <div class="columns eight offset-by-four first">
+        <div class="box">
+          <h1>DJ IN A BOX</h1>
+        </div>
+        <div class="box smaller">
+          <p><strong>Sounds:</strong> W, A, S, D, G, F</p>
+          <p><strong>Change Deck:</strong> Up, Down</p>
+          <p><strong>Stop all sound</strong> Space</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="columns four offset-by-four first">
+        <div class="box">
+          <h2>DECK</h2>
+          <span class="deck-num">1</span>
+        </div>
+      </div>
+
+      <div class="columns four">
+        <div class="box">
+          <h2>SOUND</h2>
+          <span class="sound-item"></span>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
   <?php
@@ -31,12 +62,12 @@
   <script type="text/javascript">
   <?php
     // Loop through the files
-    foreach($sounds as $file) {
-      $fileID = str_replace(".mp3", "", $file);
-      ?>
-      var <?php echo $fileID ?>sound = document.getElementById('<?php echo $fileID ?>-noise');
-      <?php
-    }
+  foreach($sounds as $file) {
+    $fileID = str_replace(".mp3", "", $file);
+    ?>
+    var <?php echo $fileID ?>sound = document.getElementById('<?php echo $fileID ?>-noise');
+    <?php
+  }
   ?>
   </script>
   <script type="text/javascript" src="js/djmakey.js"></script>
--- js/djmakey.js
+++ js/djmakey.js
@@ -1,115 +1,136 @@
 var keyW = 87, keyA = 65, keyS = 83, keyD = 68, keyF = 70, keyG = 71,
-    keyUpArrow = 38, keyLeftArrow = 37, keyRightArrow = 39, keyDownArrow = 40,
-    keySpace = 32;
+  keyUpArrow = 38, keyLeftArrow = 37, keyRightArrow = 39, keyDownArrow = 40,
+  keySpace = 32;
 var currentSoundDeck = 0;
 
 var sounds = [
-  {
-    "87": {
-      "name": 'Fade In',
-      "sound": fadeinsound,
-      "loop": false
-    },
-    "65": {
-      "name": 'Voice',
-      "sound": voicesound,
-      "loop": false
-    },
-    "83": {
-      "name": 'clap',
-      "sound": clapsound,
-      "loop": false
-    },
-    "68": {
-      "name": 'Kick',
-      "sound": kicksound,
-      "loop": false
-    },
-    "70": {
-      "name": 'invasion',
-      "sound": invasionsound,
-      "loop": false
-    },
-    "71": {
-      "name": 'melodic',
-      "sound": melodicsound,
-      "loop": false
-    }
+{
+  "87": {
+    "name": 'Fade In',
+    "sound": fadeinsound,
+    "loop": false
   },
-  {
-    "87": {
-      "name": 'Sound',
-      "sound": bass_loop_2sound,
-      "loop": false
-    },
-    "83": {
-      "name": 'Scratch',
-      "sound": techno_2sound,
-      "loop": true
-    }
+  "65": {
+    "name": 'Voice',
+    "sound": voicesound,
+    "loop": false
+  },
+  "83": {
+    "name": 'clap',
+    "sound": clapsound,
+    "loop": false
+  },
+  "68": {
+    "name": 'Kick',
+    "sound": kicksound,
+    "loop": false
+  },
+  "70": {
+    "name": 'invasion',
+    "sound": invasionsound,
+    "loop": false
+  },
+  "71": {
+    "name": 'melodic',
+    "sound": melodicsound,
+    "loop": false
   }
+},
+{
+  "87": {
+    "name": 'Bass Loop',
+    "sound": bass_loopsound,
+    "loop": true
+  },
+  "65": {
+    "name": 'Bass Loop 2',
+    "sound": bass_loop_2sound,
+    "loop": true
+  },
+  "83": {
+    "name": 'Success',
+    "sound": successsound,
+    "loop": false
+  },
+  "68": {
+    "name": 'Snares',
+    "sound": snaressound,
+    "loop": false
+  },
+  "70": {
+    "name": 'techo 1',
+    "sound": techno_1sound,
+    "loop": false
+  },
+  "71": {
+    "name": 'techno 2',
+    "sound": techno_2sound,
+    "loop": false
+  }
+}
 ];
 
 jQuery(document).ready(function($) {
-  function changeSoundDeck(keyPress, currentSoundDeck, soundsLength){
 
-    if(keyPress === keyUpArrow){
-      currentSoundDeck++;
+function changeSoundDeck(keyPress, currentSoundDeck, soundsLength){
 
-      if (currentSoundDeck >= soundsLength){
-        currentSoundDeck = 0;
-      }
-    }
+  if(keyPress === keyUpArrow){
+    currentSoundDeck++;
 
-    if(keyPress === keyDownArrow){
-      currentSoundDeck--;
-
-      if (currentSoundDeck < 0){
-        currentSoundDeck = soundsLength - 1;
-      }
-    }
-
-    return currentSoundDeck;
-  }
-
-  function playSound(keyPress, currentSoundDeck){
-    if(keyPress === keyW || keyPress === keyA || keyPress === keyS || keyPress === keyD || keyPress === keyF || keyPress === keyG){
-      sounds[currentSoundDeck][keyPress]['sound'].loop = sounds[currentSoundDeck][keyPress]['loop'];
-      sounds[currentSoundDeck][keyPress]['sound'].volume = 1;
-      sounds[currentSoundDeck][keyPress]['sound'].play();
-      $(".noise").text("Name: " + sounds[currentSoundDeck][keyPress]['name']);
+    if (currentSoundDeck >= soundsLength){
+      currentSoundDeck = 0;
     }
   }
 
-  function stopAll(){
-    $.each($('audio'), function () {
-        this.loop = false;
-        // this.stop();
-        this.volume = 0;
-        this.currentTime = 0;
-    });
+  if(keyPress === keyDownArrow){
+    currentSoundDeck--;
+
+    if (currentSoundDeck < 0){
+      currentSoundDeck = soundsLength - 1;
+    }
   }
 
+  $(".deck-num").text(currentSoundDeck+1);
 
-  $(window).keydown(function( event ) {
-    keyPress = event.keyCode;
+  return currentSoundDeck;
+}
 
-    if ( event.which == 13 ) {
-     event.preventDefault();
-    }
+function playSound(keyPress, currentSoundDeck){
+  if(keyPress === keyW || keyPress === keyA || keyPress === keyS || keyPress === keyD || keyPress === keyF || keyPress === keyG){
+    sounds[currentSoundDeck][keyPress]['sound'].loop = sounds[currentSoundDeck][keyPress]['loop'];
+    sounds[currentSoundDeck][keyPress]['sound'].volume = 1;
+    sounds[currentSoundDeck][keyPress]['sound'].play();
+    $(".sound-item").text(sounds[currentSoundDeck][keyPress]['name']);
+  }
+}
 
-    if(keyPress === keyUpArrow || keyPress === keyDownArrow){
-      currentSoundDeck = changeSoundDeck(keyPress, currentSoundDeck, sounds.length);
-    }
-
-    $(".keyCode").text(keyPress + " Sound Deck: " + currentSoundDeck);
-
-    playSound(keyPress, currentSoundDeck);
-
-    if(keyPress === keySpace){
-      stopAll();
-    }
-
+function stopAll(){
+  $.each($('audio'), function () {
+      this.loop = false;
+      // this.stop();
+      this.volume = 0;
+      this.currentTime = 0;
   });
+}
+
+
+$(window).keydown(function( event ) {
+  keyPress = event.keyCode;
+
+  if ( event.which == 13 ) {
+   event.preventDefault();
+  }
+
+  if(keyPress === keyUpArrow || keyPress === keyDownArrow){
+    currentSoundDeck = changeSoundDeck(keyPress, currentSoundDeck, sounds.length);
+  }
+
+  playSound(keyPress, currentSoundDeck);
+
+  if(keyPress === keySpace){
+    stopAll();
+  }
+
+});
 
 });
