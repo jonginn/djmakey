10f1628ff843078826027e55f5e5061cfe7b0f45
Jonathan Ginn <jon@jonginn.com>
2014-02-22 13:06:37 +0000

    WOO NOISES

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>DJ MAKEY MAKEY</title>
+  <?php
+  $dir_handle = './sound';
+  $sounds = array_diff(scandir($dir_handle), array('.', '..', 'index.php', '.DS_Store', 'dj'));
+  ?>
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+<body>
+
+  <p class="keyCode"></p>
+
+
+  <?php
+
+  // Loop through the files
+  foreach($sounds as $file) {
+    $fileID = str_replace(".mp3", "", $file);
+    ?>
+    <audio id="<?php echo $fileID ?>-noise">
+      <source src="sound/<?php echo $file ?>" type='audio/mpeg; codecs="mp3"'></source>
+    </audio>
+    <?php
+  }
+  ?>
+
+  <script type="text/javascript">
+  <?php
+    // Loop through the files
+    foreach($sounds as $file) {
+      $fileID = str_replace(".mp3", "", $file);
+      ?>
+      var <?php echo $fileID ?>sound = document.getElementById('<?php echo $fileID ?>-noise');
+      <?php
+    }
+  ?>
+  </script>
+  <script type="text/javascript" src="js/djmakey.js"></script>
+</body>
+</html>
--- js/djmakey.js
+++ js/djmakey.js
@@ -1,19 +1,32 @@
-var sound = document.getElementById('balloon-noise');
-var successSound = document.getElementById('success-noise');
-var scratchSound = document.getElementById('scratch-noise');
-
 var keyW = 87, keyA = 65, keyS = 83, keyD = 68, keyF = 70, keyG = 71,
-    keyUpArrow = 38, keyLeftArrow = 37, keyRightArrow = 39, keyDownArrow = 40;
+    keyUpArrow = 38, keyLeftArrow = 37, keyRightArrow = 39, keyDownArrow = 40,
+    keySpace = 32;
 var currentSoundDeck = 0;
 
 var sounds = [
   {
-    "87": successSound,
-    "83": scratchSound
+    "87": {
+      "name": 'Fade In',
+      "sound": fadeinsound,
+      "loop": false
+    },
+    "83": {
+      "name": 'Voice',
+      "sound": voicesound,
+      "loop": false
+    }
   },
   {
-    "87": sound,
-    "83": successSound
+    "87": {
+      "name": 'Sound',
+      "sound": bass_loop_2sound,
+      "loop": false
+    },
+    "83": {
+      "name": 'Scratch',
+      "sound": techno_2sound,
+      "loop": true
+    }
   }
 ];
 
@@ -41,8 +54,17 @@ jQuery(document).ready(function($) {
 
   function playSound(keyPress, currentSoundDeck){
     if(keyPress === keyW || keyPress === keyA || keyPress === keyS || keyPress === keyD){
-      sounds[currentSoundDeck][keyPress].play();
+      sounds[currentSoundDeck][keyPress]['sound'].loop = sounds[currentSoundDeck][keyPress]['loop'];
+      sounds[currentSoundDeck][keyPress]['sound'].play();
     }
+  }
+
+  function stopAll(){
+    $.each($('audio'), function () {
+        this.loop = false;
+        // this.stop();
+        this.currentTime = 0;
+    });
   }
 
 
@@ -60,6 +82,10 @@ jQuery(document).ready(function($) {
     $(".keyCode").text(keyPress + " Sound Deck: " + currentSoundDeck);
 
     playSound(keyPress, currentSoundDeck);
+
+    if(keyPress === keySpace){
+      stopAll();
+    }
 
   });
 
